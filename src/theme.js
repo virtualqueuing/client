61dbb9dceaac935f1b40aa46fffd35e90920ce06
Ting Huang <tinghuang.dev@gmail.com>
2022-05-23 23:39:12 +1000

    feat: add theme.js and import styled-ThemeProvoider in App

---
+++ src/theme.js
@@ -0,0 +1,11 @@
+const colors = {
+    main: '#E60012',
+    queuepage: '#E0EBEE',
+    background: '#E5E5E5',
+    fontColor: '#666666',
+    buttonGradient: 'linear-gradient(180deg, #F1E0E0 23.33%, rgba(253, 247, 247, 0) 194.67%)',
+}
+
+export default {
+    colors,
+}
