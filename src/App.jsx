318bec8c33c1d823aaaf8902241137b4cf107f1f
Ting Huang <tinghuang.dev@gmail.com>
2022-05-24 13:46:34 +1000

    perf: remove ThemeProvider in App.jsx only keep it in main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,23 @@ import React, { Suspense } from "react";
 import { Routes, BrowserRouter, Route, Navigate } from "react-router-dom";
 import router from "./router";
 import LoadingPage from "./pages/LoadingPage";
-import { ThemeProvider } from "styled-components";
-import theme from "./theme";
 
 const App = () => {
   return (
-    <>
-      <ThemeProvider theme={theme} />
-      <div className="App">
-        <Suspense fallback={<LoadingPage />}>
-          <BrowserRouter>
-            <Routes>
-              {router.map((routeConfig) => {
-                const { path } = routeConfig;
-                return <Route {...routeConfig} key={path} />;
-              })}
-              <Route path="/loading" element={<LoadingPage />} />
-              <Route path="*" element={<Navigate to="/error" replace />} />
-            </Routes>
-          </BrowserRouter>
-        </Suspense>
-      </div>
-    </>
+    <div className="App">
+      <Suspense fallback={<LoadingPage />}>
+        <BrowserRouter>
+          <Routes>
+            {router.map((routeConfig) => {
+              const { path } = routeConfig;
+              return <Route {...routeConfig} key={path} />;
+            })}
+            <Route path="/loading" element={<LoadingPage />} />
+            <Route path="*" element={<Navigate to="/error" replace />} />
+          </Routes>
+        </BrowserRouter>
+      </Suspense>
+    </div>
   );
 };
 
