bc76f4e8fa78d5a2d558f3805718b06e68e7c1fe
Ting Huang <tinghuang.dev@gmail.com>
2022-05-24 10:09:49 +1000

    style: update new css colors varialbes correspondence with new queuing design

--- src/theme.js
+++ src/theme.js
@@ -1,8 +1,13 @@
 const colors = {
     main: '#E60012',
-    queuepage: '#E0EBEE',
+    queuePage: '#E0EBEE',
     background: '#E5E5E5',
     fontColor: '#666666',
+    deleteColor:'#DD0000',
+    completeColor:'#5D5670',
+    statusW:'#FFD25D',
+    statusA:'#DD0000',
+    statusC:'#13E800',
     buttonGradient: 'linear-gradient(180deg, #F1E0E0 23.33%, rgba(253, 247, 247, 0) 194.67%)',
 }
 
