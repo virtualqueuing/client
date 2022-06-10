9949baa6865a891a0ea37fa5a906fa7d53cbee22
Matt <tianyuma1508977@gmail.com>
2022-06-10 06:31:09 +0000

    Merged in VQ-57-connect-backend-data-to-show-every (pull request #30)
    Add Table Type fiter in table, adjust leftmenu Note format
    
    * Add Table Type fiter in table, adjust leftmenu Note format
    
    
    Approved-by: Yangsu Wang

--- src/components/RightMenu/RightMenu.jsx
+++ src/components/RightMenu/RightMenu.jsx
@@ -8,6 +8,7 @@ import axios from "axios";
 const MainBar = () => {
   const [queueStatus, setQueueStatus] = useState("All");
   const [queues, setQueues] = useState([]);
+  const [tableType, setTableType] = useState("Table Type");
 
   useEffect(() => {
     const fetchQueue = async () => {
@@ -20,8 +21,8 @@ const MainBar = () => {
 
   return (
     <RightMenuContainer>
-      <Header queueStatus={queueStatus} setQueueStatus={setQueueStatus} />
-      <MainQueues queueStatus={queueStatus} queues={queues} />
+      <Header queueStatus={queueStatus} setQueueStatus={setQueueStatus} setTableType={setTableType}/>
+      <MainQueues queueStatus={queueStatus} tableType={tableType} queues={queues} />
       <Footer />
     </RightMenuContainer>
   );
