b545cf97f2075b9b5530671a0bee5d5310d81da4
Haomin Xin <haomin.working@gmail.com>
2022-06-12 12:04:42 +1000

    docs change variable's name

--- src/components/AddNew/InputSelect.jsx
+++ src/components/AddNew/InputSelect.jsx
@@ -8,8 +8,8 @@ import React, { useState } from 'react';
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
 const InputSelect = () => {
-  const [people, setPeople] = useState();
-  function getType(num) {
+  const [guestNum, setGuestNum] = useState();
+  function getTableType(guests) {
     const tableType = {
       "1": "Small",
       "2": "Small",
@@ -24,12 +24,12 @@ const InputSelect = () => {
       "11": "Large",
       "12": "Large",
     }
-    return tableType[num];
+    return tableType[guests];
   }
   return (
     <StyledInputSelectFlex>
       <CustomStyledInputSelectImage>
-        <CustomStyledInputSelectBg name="guestsNumber" value={people} onChange={e=>setPeople(e.target.value)}>
+        <CustomStyledInputSelectBg name="guestsNumber" value={guestNum} onChange={e=>setGuestNum(e.target.value)}>
           <option disabled selected hidden>
             People
           </option>
@@ -40,7 +40,7 @@ const InputSelect = () => {
       </CustomStyledInputSelectImage>
       <CustomStyledInputSelectBg name="tableSize">
         <option value="title" disabled selected hidden>
-          {getType(people)}
+          {getTableType(guestNum)}
         </option>
       </CustomStyledInputSelectBg>
     </StyledInputSelectFlex>
