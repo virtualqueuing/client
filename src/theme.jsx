b5e2f3fa35893a88e3af89892ab959bbd518b2a6
Yangsu Wang <wanyansuroy@gmail.com>
2022-06-15 15:40:52 +0000

    Merged in VQ-85-update-theme-js-for-color-palette (pull request #37)
    feat: updated useMemo dependencies and theme file
    
    * feat: updated useMemo dependencies and theme file

--- src/theme.jsx
+++ src/theme.jsx
@@ -1,15 +1,48 @@
 const colors = {
-  main: "#E60012",
-  queuePage: "#E0EBEE",
-  background: "#E5E5E5",
-  fontColor: "#666666",
-  deleteColor: "#DD0000",
-  completeColor: "#5D5670",
-  statusW: "#FFD25D",
-  statusA: "#DD0000",
-  statusC: "#13E800",
-  inputText: "#7B5D5D",
-  addNewbg: "#EF5965",
+  components: {
+    positiveButton: {
+      background: {
+        color: "#2EAD7C",
+        opacity: 0.1,
+      },
+      fontColor: "#2EAD7C",
+    },
+    negativeButton: {
+      background: "#FE3F7F",
+      fontColor: "#E54848",
+    },
+    neutralButton: {
+      background: "#FFFDCD",
+      fontColor: "#FFAB5D",
+    },
+    arrivalButton: {
+      fontColor: "#5F5186",
+      borderColor: "#5D5670",
+    },
+    absentButton: {
+      fontColor: "#E74C3C",
+      borderColor: "#E60012",
+    },
+    tags: {
+      birthdayTag: "#FFF2AF",
+      wheelchairTag: "#AFE2FF",
+      highchairTag: "#E1FFAF",
+      tagColorList: [
+        "#86E3CE", "#D0E6A5", "#FFDD94", "#FA897B", "#CCABDB", "#B5EAD7", "#E2F0CB"
+      ]
+    }
+  },
+  page: {
+    main: "#77A48E",
+    secondary: "#EDF4F4",
+  },
+  fonts: {
+    main: "#514C55",
+    secondary: "#666666",
+    activeRoute: "#516D98",
+    inactiveRoute: "#C4C4C4",
+    inactiveMenu: "#9A9A9A",
+  }
 };
 
 const fontSizes = {
