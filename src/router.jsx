9090e6e037e887d50a657bf667845a8e9b3f7469
Tianyu <tianyuma1508977@gmail.com>
2022-06-16 11:56:33 +1000

    upload icons of new UI

--- src/router.jsx
+++ src/router.jsx
@@ -3,6 +3,7 @@ import React from "react";
 const HomePage = React.lazy(() => import("./pages/Homepage"));
 const ErrorPage = React.lazy(() => import("./pages/Errorpage"));
 const AddNewPage = React.lazy(() => import("./pages/AddNewPage"));
+const LoginPage = React.lazy(() => import("./pages/LoginPage"));
 
 const routes = [
   {
@@ -17,6 +18,10 @@ const routes = [
   {
     path: "/addnew",
     element: <AddNewPage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
   },
 ];
 
