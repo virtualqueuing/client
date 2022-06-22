8b6964dccf50e81b1df32eb0eaee010fc5892a3b
Yangsu Wang <wanyansuroy@gmail.com>
2022-06-22 16:47:04 +1000

    feat: added active queue context provider

--- src/pages/Context.jsx
+++ src/pages/Context.jsx
@@ -1,2 +1,12 @@
-import React from "react";
-export const Context = React.createContext();
+import React, { useState, createContext } from "react";
+export const Context = createContext();
+
+const defaultActiveQueue = {};
+
+export const activeQueueContext = createContext(defaultActiveQueue);
+
+export const ActiveQueueProvider = (props) => {
+  const [state, setState] = useState(defaultActiveQueue);
+
+  return <activeQueueContext.Provider value={state}>{props.children}</activeQueueContext.Provider>;
+};
