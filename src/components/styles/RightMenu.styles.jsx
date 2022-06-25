cfc5c21525b41976be36c197c734829fd407e2eb
Haomin Xin <haomin.working@gmail.com>
2022-06-26 00:50:26 +1000

    docs: update left menu and right menu UI

--- src/components/styles/RightMenu.styles.jsx
+++ src/components/styles/RightMenu.styles.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const RightMenuContainer = styled.div`
-  width: 73vw;
+  width: 85vw;
   height: 100vh;
   display: flex;
   flex-direction: column;
