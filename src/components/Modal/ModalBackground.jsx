efcfe2b3ae74e2e1bae204b6f341496609d97e3c
Roy Wang <wanyansuroy@gmail.com>
2022-06-30 02:11:04 +1000

    feat: created arrival modal added Jenkinsfile updated constants

---
+++ src/components/Modal/ModalBackground.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const ModalBackground = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(200, 200, 200);
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
