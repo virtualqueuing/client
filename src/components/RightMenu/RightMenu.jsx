1e8d00bfebfdf42d10cb1ceaaeea7d295a4c9873
Stephy <stephyliang2016@gmail.com>
2022-07-04 10:07:22 +0000

    Merged in version3-VQ-126-filter-out-the-first-customers-name (pull request #84)
    feat:filter-out-the-first-customers-name and queue number to display on left menu
    
    * feat:filter-out-the-first-customers-name and queue number to display on left menu
    
    
    Approved-by: Yangsu Wang

--- src/components/RightMenu/RightMenu.jsx
+++ src/components/RightMenu/RightMenu.jsx
@@ -6,9 +6,8 @@ import { RightMenuContainer } from "../styles/RightMenu.styles";
 import axios from "axios";
 import { API_URI } from "../../constant.jsx";
 
-const MainBar = () => {
+const MainBar = ({ queues, setQueues }) => {
   const [queueStatus, setQueueStatus] = useState("All");
-  const [queues, setQueues] = useState([]);
   const [tableType, setTableType] = useState("Table Type");
   const [searchQueue, setSearchQueue] = useState("");
 
