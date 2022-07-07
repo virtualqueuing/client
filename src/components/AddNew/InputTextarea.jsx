d566d2397518b1d0f52cac80d910e9e40e0fa875
Joe Zhou <peelzhou@outlook.com>
2022-07-07 19:05:51 +1000

    fix: edit guest show tags

--- src/components/AddNew/InputTextarea.jsx
+++ src/components/AddNew/InputTextarea.jsx
@@ -6,8 +6,11 @@ import { StyledTextarea } from "../styles/AddNew.styles";
 
 const InputTextarea = ({ inputInfo }) => {
   // click Tag to create new tag in CreateTags
-  const existingNotes = inputInfo?.notes[0].split(",") || null;
-  const [tags, setTags] = useState(existingNotes || []);
+  const noteString = inputInfo?.notes[0].split(",");
+  const cleanedNotes = noteString.filter((s) =>{
+    return s && s.trim();
+  })
+  const [tags, setTags] = useState(cleanedNotes);
   const ref = useRef(null);
 
   const SuggestedTag = (e) => {
