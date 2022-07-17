1e464eaeb5d785ceef20ea2f6f70eee6f29ec973
Yangsu Wang <wanyansuroy@gmail.com>
2022-07-17 14:34:11 +0000

    Merged in UI-fix (pull request #134)
    feat: changed UI for buttons and added animations
    
    * feat: changed UI for buttons and added animations

--- src/components/RightMenu/Footer.jsx
+++ src/components/RightMenu/Footer.jsx
@@ -5,6 +5,16 @@ import { useState } from "react";
 import { showNewFormContext } from "../../pages/Context";
 
 const buttonVariants = {
+  before: { y: "100vh" },
+  after: {
+     y: 0,
+     transition: {
+      delay: 0.1,
+      type: "spring",
+      stiffness: 30,
+      damping: 8
+     }
+    },
   hover: {
     scale: 1.1,
     textShadow: "0px, 0px, 8px, rgb(255,255,255)",
@@ -21,7 +31,7 @@ const Footer = () => {
     <showNewFormContext.Provider value={{ setShowAddNewForm }}>
       {showAddNewForm && <AddNewPage setShowAddNewForm={setShowAddNewForm} />}
       <StyledFooter>
-        <AddGuestButton variants={buttonVariants} whileHover="hover" onClick={() => setShowAddNewForm(true)}>Add Guest</AddGuestButton>
+        <AddGuestButton variants={buttonVariants} initial="before" animate="after" whileHover="hover" onClick={() => setShowAddNewForm(true)}>Add Guest</AddGuestButton>
       </StyledFooter>
     </showNewFormContext.Provider>
   );
