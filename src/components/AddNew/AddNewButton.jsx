323b6c0e61a7831019e928ccb9cb09b94bebd6cb
harris.ywang <harris.ywang@outlook.com>
2022-07-18 14:22:56 +0000

    Merged in add-tests (pull request #137)
    test: add some tests
    
    * test: add some tests
    
    * Merged main into add-tests
    
    Approved-by: Yangsu Wang

--- src/components/AddNew/AddNewButton.jsx
+++ src/components/AddNew/AddNewButton.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import React from "react";
 import { showNewFormContext } from "../../pages/Context";
 import { useContext } from "react";
 import { motion } from "framer-motion";
