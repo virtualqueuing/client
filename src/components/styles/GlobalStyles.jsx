e97f79aed36e4ea2925640975456d36b9935b132
Ting Huang <tinghuang.dev@gmail.com>
2022-07-28 22:44:11 +1000

    fix: merge conflict

--- src/components/styles/GlobalStyles.jsx
+++ src/components/styles/GlobalStyles.jsx
@@ -7,15 +7,7 @@ const GlobalStyles = createGlobalStyle`
     
     body {
         margin: 0;
-<<<<<<< HEAD
-        font-family: Arial, Helvetica, sans-serif;
-        /* overflow-x:hidden; */
-||||||| 69dd0b0
-        font-family: Arial, Helvetica, sans-serif;
-=======
         font-family: 'Poppins', sans-serif;
-        /* overflow-x:hidden; */
->>>>>>> main
     }
 `;
 
