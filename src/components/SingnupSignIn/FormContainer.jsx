13fec14491bc243ca8b0377c7bb352a8225e8843
Roy Wang <wanyansuroy@gmail.com>
2022-08-07 15:34:55 +1000

    ui: updated signup and login page background

---
+++ src/components/SingnupSignIn/FormContainer.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const FormContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+display: flex;
+align-items: center;
+justify-content: center;
+background-color: #fff;
+`
+
+export const BackgroundIMG = styled.img`
+  opacity: 0.5;
+  width: 80%;
+  height: auto;
+object-fit: cover;
+`
