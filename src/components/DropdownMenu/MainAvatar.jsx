27baad466770c11099976ebf75155966a0a23cc8
Yangsu Wang <wanyansuroy@gmail.com>
2022-08-08 16:09:52 +0000

    Merged in style-leftmenu-ui-hover (pull request #165)
    style: updated UI and added dropdown menu code
    
    * style: updated UI and added dropdown menu code

---
+++ src/components/DropdownMenu/MainAvatar.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const MainAvatar = styled.img`
+  position: absolute;
+  top: 20px;
+  right: 20px;
+  height: 60px;
+  width: 60px;
+  border-radius: 50%;
+  overflow: hidden;
+  cursor: pointer;
+`;
