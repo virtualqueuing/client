9a22e8b84698bb6a5267e43be2fed4f2330fb5dd
Yangsu Wang <wanyansuroy@gmail.com>
2022-08-14 14:00:12 +0000

    Merged in ci-bug-about-us-update (pull request #171)
    ci: updated ci bug and about us page
    
    * ci: updated ci bug and about us page

--- src/components/MainHomepage/AboutUsTeams.jsx
+++ src/components/MainHomepage/AboutUsTeams.jsx
@@ -11,21 +11,21 @@ import joePhoto from "../../assets/avatar/joe-resize.jpg";
 
 export const Developers = [
   {
-    name: "Alfred",
+    name: "Alfred Xin",
     position: "Full-stack Developer",
     avatar: alfredPhoto,
     linkedIn: "https://www.linkedin.com/in/haomin-xin/",
     email: "mailto:haomin.working@gmail.com",
   },
   {
-    name: "Joe",
+    name: "Joe Zhou",
     position: "Full-stack Developer",
     avatar: joePhoto,
     linkedIn: "https://www.linkedin.com/in/hanqing-zhou/",
     email: "mailto:peelzhou@outlook.com",
   },
   {
-    name: "Roy",
+    name: "Roy Wang",
     position: "Full-stack Developer",
     avatar: royPhoto,
     linkedIn: "https://www.linkedin.com/in/yangsuwang/",
@@ -33,21 +33,21 @@ export const Developers = [
   },
 
   {
-    name: "Stephy",
+    name: "Stephy Liang",
     position: "Full-stack Developer",
     avatar: stephyPhoto,
     linkedIn: "https://www.linkedin.com/in/stephy-liang/",
     email: "mailto:stephyliang2016@gmail.com",
   },
   {
-    name: "Tianyu",
+    name: "Tianyu Ma",
     position: "Full-stack Developer",
     avatar: tianyuPhoto,
     linkedIn: "https://www.linkedin.com/in/tianyu-ma0923/",
     email: "mailto:tianyuma1508977@gmail.com",
   },
   {
-    name: "Ting",
+    name: "Ting Huang",
     position: "Full-stack Developer",
     avatar: tingPhoto,
     linkedIn: "https://www.linkedin.com/in/tinghuang-dev/",
@@ -57,28 +57,28 @@ export const Developers = [
 
 export const DevOps = [
   {
-    name: "Elva",
+    name: "Elva Li",
     position: "Business Analyst",
     avatar: elvaPhoto,
     linkedIn: "https://www.linkedin.com/in/elvalpw/",
     email: "mailto:lpwsea@gmail.com",
   },
   {
-    name: "Edward",
+    name: "Edward Chen",
     position: "DevOps",
     avatar: edwardPhoto,
     linkedIn: "https://www.linkedin.com/in/edward-chen-au/",
     email: "mailto:edwardchen89@hotmail.com",
   },
   {
-    name: "Jessie",
+    name: "Jessie Li",
     position: "DevOps",
     avatar: jessiePhoto,
     linkedIn: "https://www.linkedin.com/in/xianhua-li-b0a953155/",
     email: "mailto:lixianhua0920@gmail.com",
   },
   {
-    name: "Leo",
+    name: "Leo Guo",
     position: "DevOps",
     avatar: leoPhoto,
     linkedIn: "https://www.linkedin.com/in/leo-guo",
